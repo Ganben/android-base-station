3010e37e89a4b89c1a43b69b9ad8e6b9ec46b909
ganben <caozhao@gmail.com>
2016-11-24 16:44:29 +0800

    :fire:rm yunba:sparkles: intro paho mqtt

--- app/src/main/java/net/chaoc/blescanner/MainActivity.java
+++ app/src/main/java/net/chaoc/blescanner/MainActivity.java
@@ -34,17 +34,30 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 
 import java.io.ByteArrayInputStream;
-
-import io.yunba.android.manager.YunBaManager;
+import java.net.URISyntaxException;
 
 import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity implements android.view.View.OnClickListener {
 
+    MqttAndroidClient sampleClient;
     BluetoothManager btManager;
     BluetoothAdapter btAdapter;
     BluetoothLeScanner btScanner;
@@ -60,6 +73,14 @@ public class MainActivity extends AppCompatActivity implements android.view.View
     private MessageReceiver mMessageReceiver;
     public final static String MESSAGE_RECEIVED_ACTION = "io.yunba.example.msg_received_action";
     AdvertiseCallback advCallback;
+    //mqtt parameter
+    String topic        = "test";
+    String content      = "Message from MqttPublishSample";
+    int qos             = 1;
+    String serverUri       = "tcp://47.88.15.107:1883";
+    String clientId     = "AndroidSample";
+    long matchCount = 0;
+    long otherCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,53 +105,26 @@ public class MainActivity extends AppCompatActivity implements android.view.View
             }
         });
         stopScanningButton.setVisibility(View.INVISIBLE);
-
-        YunBaManager.start(getApplicationContext());
-        YunBaManager.subscribe(getApplicationContext(), new String[]{"t1"}, new IMqttActionListener() {
-
-            @Override
-            public void onSuccess(IMqttToken arg0) {
-                Log.d("main", "Subscribe topic succeed");
-
-            }
-
-            @Override
-            public void onFailure(IMqttToken arg0, Throwable arg1) {
-                Log.d("main", "Subscribe topic failed" );
-            }
-        });
-
-        registerMessageReceiver();
-
     }
 
-    public void registerMessageReceiver(){
-
-        mMessageReceiver = new MessageReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(YunBaManager.MESSAGE_RECEIVED_ACTION);
-        filter.addCategory(getPackageName());
-        registerReceiver(mMessageReceiver, filter);
-
-        IntentFilter filterCon = new IntentFilter();
-        filterCon.addAction(YunBaManager.MESSAGE_CONNECTED_ACTION);
-        filterCon.addCategory(getPackageName());
-        registerReceiver(mMessageReceiver, filterCon);
-
-        IntentFilter filterDis = new IntentFilter();
-        filterDis.addAction(YunBaManager.MESSAGE_DISCONNECTED_ACTION);
-        filterDis.addCategory(getPackageName());
-        registerReceiver(mMessageReceiver, filterDis);
-
-        IntentFilter pres = new IntentFilter();
-        pres.addAction(YunBaManager.PRESENCE_RECEIVED_ACTION);
-        pres.addCategory(getPackageName());
-        registerReceiver(mMessageReceiver, pres);
-
-    }
     @Override
     public void onClick(View v) {
+    }
+
+    public void sent(){
 //        Toast.makeText(getApplicationContext())
+//        mSocket.emit("new message", "123123");
+        Log.w(this.getClass().getSimpleName(), "view clicked");
+        String s = "this me , this is me!";
+        byte[] bs = s.getBytes();
+        MqttMessage msg = new MqttMessage();
+        msg.setPayload(bs);
+        try {
+            sampleClient.publish(topic, msg);
+        } catch (MqttException me) {
+            Log.w("sent error", "me ".concat(me.getMessage().toString()));
+        }
+        Toast.makeText(this.getApplicationContext(), "clicked", Toast.LENGTH_SHORT);
     }
 
     public void init(){
@@ -187,6 +181,7 @@ public class MainActivity extends AppCompatActivity implements android.view.View
                     //start advertising;
                     advertise(toByteArray(advDataStr));
 
+
                 } else {
                     //disabled
                     //stop advertising;
@@ -195,25 +190,140 @@ public class MainActivity extends AppCompatActivity implements android.view.View
             }
         });
 
-        // Make sure we have access coarse location enabled, if not, prompt the user to enable it
-//        if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//            builder.setTitle("This app needs location access");
-//            builder.setMessage("Please grant location access so this app can detect peripherals.");
-//            builder.setPositiveButton(android.R.string.ok, null);
-//            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-//                @Override
-//                public void onDismiss(DialogInterface dialog) {
-//                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
-//                }
-//            });
-//            builder.show();
-//        }
+
+        mqtt_init();
+
+
+        //use socket io;
+//        mSocket.connect();
+        //
+        //
+        Log.w(this.getClass().getSimpleName(), String.valueOf(sampleClient.isConnected()));
+    }
+
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("tcp://47.88.15.107:1883");
+        } catch (URISyntaxException e) {}
+    }
+
+
+    public void mqtt_init(){
+
+//        MemoryPersistence persistence = new MemoryPersistence();
+        sampleClient = new MqttAndroidClient(this.getApplicationContext(), serverUri, clientId);
+
+        sampleClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean reconnect, String serverURI) {
+
+                if (reconnect) {
+//                    addToHistory("Reconnected to : " + serverURI);
+                    // Because Clean Session is true, we need to re-subscribe
+                    subscribeToTopic();
+                } else {
+//                    addToHistory("Connected to: " + serverURI);
+                }
+            }
+
+            @Override
+            public void connectionLost(Throwable cause) {
+//                addToHistory("The Connection was lost.");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+//                addToHistory("Incoming message: " + new String(message.getPayload()));
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        });
+
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+        try {
+            //addToHistory("Connecting to " + serverUri);
+            sampleClient.connect(mqttConnectOptions, this.getApplicationContext(), new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    sampleClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+//                    addToHistory("Failed to connect to: " + serverUri);
+                }
+            });
+
+
+        } catch (MqttException ex){
+            ex.printStackTrace();
+        }
+
+        try {
+            MqttMessage msg = new MqttMessage();
+            byte[] bt = new byte[]{ 1, 2, 3, 4, 5};
+
+            msg.setPayload(bt);
+            msg.setId(1);
+            msg.setQos(1);
+            topic = "test";
+//            sampleClient.publish(topic, msg);
+            sampleClient.getBufferedMessage(1);
+        } catch (Exception me) {
+            Log.w(this.getClass().getSimpleName(), "-----------publich");
+        }
+
+    }
+
+    public void subscribeToTopic(){
+        try {
+            sampleClient.subscribe("test", 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+//                    addToHistory("Subscribed!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+//                    addToHistory("Failed to subscribe");
+                }
+            });
+
+            // THIS DOES NOT WORK!
+            sampleClient.subscribe("test", 0, new IMqttMessageListener() {
+                @Override
+                public void messageArrived(String topic, MqttMessage message) throws Exception {
+                    // message Arrived!
+                    System.out.println("Message: " + topic + " : " + new String(message.getPayload()));
+                }
+            });
+
+        } catch (MqttException ex){
+            System.err.println("Exception whilst subscribing");
+            ex.printStackTrace();
+        }
+
     }
 
     public void startScanning() {
         System.out.println("start scanning");
         peripheralTextView.setText("");
+
+        sent();
         startScanningButton.setVisibility(View.INVISIBLE);
         stopScanningButton.setVisibility(View.VISIBLE);
         AsyncTask.execute(new Runnable() {
@@ -242,13 +352,30 @@ public class MainActivity extends AppCompatActivity implements android.view.View
         private ScanCallback leScanCallback = new ScanCallback() {
             @Override
             public void onScanResult(int callbackType, ScanResult result) {
-                peripheralTextView.append("Name: " + result.getDevice().getName() + " rssi: " + result.getRssi() + " : " + result.getScanRecord().getManufacturerSpecificData().keyAt(0) + "\n" + toHexString(result.getScanRecord().getManufacturerSpecificData().valueAt(0))+ "\n");
+                if (result.getScanRecord().getManufacturerSpecificData().keyAt(0) == 65280) {
+                    matchCount += 1;
+                    try {
+//                    sampleClient = new MqttClient(broker, clientId);
+//                    sampleClient.connect();
+                        MqttMessage msg = new MqttMessage();
+                        msg.setQos(1);
+                        msg.setPayload(result.getScanRecord().getManufacturerSpecificData().valueAt(0));
 
-                // auto scroll for text view
-                final int scrollAmount = peripheralTextView.getLayout().getLineTop(peripheralTextView.getLineCount()) - peripheralTextView.getHeight();
-                // if there is no need to scroll, scrollAmount will be <=0
-                if (scrollAmount > 0)
-                    peripheralTextView.scrollTo(0, scrollAmount);
+                        sampleClient.publish("test", msg);
+                    } catch (MqttException me) {
+                        Log.d(this.getClass().getSimpleName(), "mqtt sent err");
+                    }
+
+                    peripheralTextView.append("$" + matchCount + ":N:" + result.getDevice().getName() + " rssi: " + result.getRssi() + " : " + result.getScanRecord().getManufacturerSpecificData().keyAt(0) + "\n" + toHexString(result.getScanRecord().getManufacturerSpecificData().valueAt(0)) + "\n");
+
+                    // auto scroll for text view
+                    final int scrollAmount = peripheralTextView.getLayout().getLineTop(peripheralTextView.getLineCount()) - peripheralTextView.getHeight();
+                    // if there is no need to scroll, scrollAmount will be <=0
+                    if (scrollAmount > 0)
+                        peripheralTextView.scrollTo(0, scrollAmount);
+                } else {
+                    otherCount += 1;
+                }
             }
         };
 
